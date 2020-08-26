8f41abe11fa9b6549589b74a4171ba80334540d0
Catherine Gloria <catherinegloria@gmail.com>
2020-08-26 14:53:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#move
+#valid_move?
+#position_taken?
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
