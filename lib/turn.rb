08e94cd53c42fd19ef0c029f77d6facfc11c1b12
Catherine Gloria <catherinegloria@gmail.com>
2020-08-26 14:40:42 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#move
+
+def turn (get input)
+  convert input to index
+  if index is valid
+    make the move for input
+  else
+    ask for input again until you get a valid input
+  end
+end
